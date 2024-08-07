8b6471a5da12096f89490c968d39c87f51c90944
litang0908 <freetyc@foxmail.com>
2024-08-07 09:04:42 +0800

    MOD：ext 项目配置更改

--- charts_flutter/example/ios/Flutter/flutter_export_environment.sh
+++ charts_flutter/example/ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/tangyc/Documents/softs2/flutter"
+export "FLUTTER_ROOT=/Users/tangyc/Documents/softs2/next_flutter_src"
 export "FLUTTER_APPLICATION_PATH=/Users/tangyc/Documents/codes/qianji_flutter/ext/charts/charts_flutter/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
